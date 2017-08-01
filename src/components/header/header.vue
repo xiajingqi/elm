e369df9a525887bac6a1e8fab8dd49c26ad8533a
夏靖奇 <xiajingqi@jiuchunjiaoyu.com>
2017-08-01 17:32:10 +0800

    8.1-01

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,32 +13,194 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div v-if="seller.supports" class="support">
-                    <span class="icon"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
-            </div>           
+            </div>
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
+                <span class="count">{{seller.supports.length}}个</span>
+                <i class="iconfont icon-keyboard_arrow_right"></i>
+            </div>      
         </div>
-        <div class="bulletin-wrapper">
-            
+        <div class="bulletin-wrapper" @click="showDetail">
+            <span class="title"></span><span class="text">{{seller.bulletin}}</span>
+            <i class="iconfont icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" alt="">
+        </div>
+        <div class="detail" v-show="detailShow">
+            <div class="wrapper clearfix">
+                <div class="main">
+                    <h1 class="name">{{seller.name}}</h1>
+                </div>
+            </div>
+            <div class="close">
+                <i class="iconfont icon-close"></i>
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
     props:['seller'],
+    data(){
+        return{
+            classMap:['decrease','discount','special','invoice','guarantee'],
+            detailShow:false
+        }
+    },
+    created(){
+        // this.classMap=['decrease','discount','special','invoice','guarantee']
+    },
+    methods:{
+        showDetail(){
+            this.detailShow=true
+        }
+    }
     
 }
 </script>
 <style lang="stylus" scoped>
+    @import '../../common/stylus/mixin.styl'
     .header
+        position relative
+        overflow hidden
         color #fff
-        background-color #000
-        .content-wrapper           
+        background rgba(7,17,27,.5)
+        .content-wrapper
+            position relative          
             padding 24px 12px 18px 24px
+            font-size 0px;
             .avatar
                 display inline-block
+                vertical-align top
+                img
+                    border-radius 2px
             .content
                 display inline-block
+                margin-left 16px
+                .title
+                    margin 2px 0 8px 0
+                    .brand
+                        display inline-block
+                        vertical-align top
+                        width 30px
+                        height 18px
+                        bg('brand')
+                        background-size 30px 18px
+                        background-repeat no-repeat
+                    .name
+                        margin-left 6px
+                        font-size 16px
+                        line-height 18px
+                        font-weight bold
+                .des
+                    margin-bottom 10px
+                    line-height 12px
+                    font-size 12px
+                .support
+                    .icon
+                        display inline-block
+                        vertical-align top
+                        width 12px
+                        height 12px
+                        margin-right 4px
+                        background-size 12px 12px
+                        background-repeat no-repeat
+                        &.decrease
+                            bg('decrease_1')
+                        &.discount
+                            bg('discount_1')
+                        &.guarantee
+                            bg('discount_1')
+                        &.invoice
+                            bg('invoice_1')
+                        &.special
+                            bg('special_1')
+                    .text
+                        line-height 12px
+                        font-size 10px
+            .support-count
+                position absolute
+                right 12px
+                bottom 18px
+                padding 0 8px
+                height 24px
+                line-height 24px
+                border-radius 14px
+                background #999
+                text-align center     
+                .count
+                    vertical-align top
+                    font-size 10px
+                .icon-keyboard_arrow_right
+                    color #fff
+        .bulletin-wrapper
+            position relative
+            height 28px
+            line-height 28px
+            padding 0 22px 0 12px
+            white-space nowrap
+            overflow hidden
+            text-overflow ellipsis
+            background: rgba(7,17,27,0.2);
+            .title
+                display inline-block
+                margin-top 7px
+                margin 0 4px
+                width 22px
+                height 12px
+                bg('bulletin')
+                background-size 22px 12px
+                background-repeat no-repeat
+            .text
+                vertical-align top
+                font-size 10px
+            .icon-keyboard_arrow_right
+                position absolute
+                right 12px
+                top 1px
+        .background
+            position absolute
+            top 0
+            left 0
+            width 100%
+            height 100%
+            z-index -1
+            filter blur(10px)
+            img
+                width 100%
+                height 100%
+        .detail
+            position fixed
+            z-index 10
+            top 0
+            left 0
+            width 100%
+            height 100%
+            overflow auto
+            background rgba(7,17,27,.8)
+            .wrapper
+                min-height 100%
+                width 100%
+                .main
+                    margin-top 64px
+                    padding-bottom 64px
+                    .name
+                        line-height 16px
+                        text-align center
+                        font-size 16px
+                        font-weight 600
+            .close
+                position relative
+                width 32px
+                height 32px
+                margin -64px auto 0 auto
+                // clear both
+                text-align center
+                    // font-size 32px
+
 
 </style>
 
---
+++ src/components/star/star.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="star" :class="starType">
+        <span v-for="(value,index) in itemClasses" :class="itemClass" :key="index"></span>
+  </div>
+</template>
+<script>
+const length=5;
+const cls_on=on;
+const cls_half=half;
+const cls_off=off;
+export default {
+    props:['size','score'],
+    computed:{
+        starType(){
+            return 'star-'+this.size;
+        },
+        itemClass(){
+            let result=[];
+            let score = Math.floor(this.score*2)/2;
+            let has =score %1!==0;
+            let integer=Math.floor(score);
+            for(let i=0;i<integer;i++){
+                result.push(cls_on)
+            }
+            if(has){
+                result.push(cls_half)
+            }
+            while(result.length<length){
+                result.push(cls_off)
+            }
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import '../../common/stylus/mixin.styl'
+    .star
+        .star-item
+            display inline-block
+            background-repeat no-repeat
+        &.star-48
+            .star-item
+                width 20px
+                height 20px
+                margin-right 22px
+                background-size 20px 20px
+                &:last-child
+                    margin-right 0
+                &.on
+                    bg('star48_on')
+                &.half
+                    bg('star48_half')
+                &.off
+                    bg('star48_off')
+
+        &.star-36
+            .star-item
+                width 15px
+                height 15px
+                margin-right 6px
+                background-size 15px 15px
+                &:last-child
+                    margin-right 0
+                &.on
+                    bg('star36_on')
+                &.half
+                    bg('star36_half')
+                &.off
+                    bg('star36_off')
+        &.star-24
+            .star-item
+                width 10px
+                height 10px
+                margin-right 3px
+                background-size 10px 10px
+                &:last-child
+                    margin-right 0
+                &.on
+                    bg('star24_on')
+                &.half
+                    bg('star24_half')
+                &.off
+                    bg('star24_off')
+
+</style>
